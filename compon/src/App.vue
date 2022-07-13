c9badacfcd8962012071d8fe1e93c22b323fc552
Vitalii Hoida <44941430+VitaliiHoida@users.noreply.github.com>
2022-07-13 13:34:34 +0300

    comp init

---
+++ compon/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <app-button :icon="require('@/assets/logo.png')" bordered></app-button>
+    <hr/>
+    <app-button :name="'Reset Filter'" orange></app-button>
+    <hr/>
+    <app-button :name="'chekout'" uppercase bordered></app-button>
+    <hr/>
+    <app-button ></app-button>
+  </div>
+</template>
+
+<script>
+import AppButton from './components/AppButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppButton
+  }
+}
+</script>
+
+<style>
+.wrapper{
+  padding: 100 px;
+  max-width: 700px;
+  margin: 0 auto;
+}
+</style>
---
+++ compon/src/components/AppButton.vue
@@ -0,0 +1,106 @@
+<template>
+    <button type="button" 
+            @click="click" 
+            :class="{'colored': colored, 
+                     'uppercase': uppercase,
+                     'bordered' : bordered,
+                     'orange' : orange,
+                     'icon' : icon.length>0}">
+        {{name}}
+        <img v-if="icon.length>0" :src="icon">
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'AppButton',
+    props:{
+        name: {
+            type: String,
+            default: ''
+        },
+        icon: {
+            type: String,
+            default: ''
+        },
+        uppercase:{
+            type: Boolean,
+            default: false
+        },
+        colored:{
+            type: Boolean,
+            default: false
+        },
+        filter:{
+            type: Boolean,
+            default: false
+        },
+        bordered:{
+            type: Boolean,
+            default: false
+        },
+        orange:{
+            type: Boolean,
+            default: false
+        },
+    },
+    methods:{
+        click(){
+            this.$emit('btnClick');
+        }
+    }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap');
+
+button{
+font-family: 'Lato';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+line-height: 22px;
+color: #7B7B7B;
+background: none;
+border: none;
+padding: 0;
+margin: 0;
+}
+button:hover, button:active{
+    cursor: pointer;
+    color:#FF7020;
+}
+
+.orange{
+    color: #FF7020;
+}
+
+.uppercase{
+    text-transform: uppercase;
+}
+
+.icon img{
+    max-width: 22px;
+}
+
+.icon {
+    max-width: 60px;
+    max-height: 56px;
+}
+
+.icon:hover{
+    border-color: #FF7020;
+}
+
+.icon:active{
+    background-color: #FF7020;
+}
+
+.bordered {
+    border: 1px solid #D8D8D8;
+    border-radius: 12px;
+    padding: 19px;
+    
+}
+</style>

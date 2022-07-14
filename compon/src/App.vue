dc44d08c3d0d9c1447156eb825fe9bc70a5cb3ab
Vitalii Hoida <44941430+VitaliiHoida@users.noreply.github.com>
2022-07-14 11:14:48 +0300

    компонент кнопки

--- compon/src/App.vue
+++ compon/src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="wrapper">
-    <app-button :icon="require('@/assets/logo.png')" bordered></app-button>
+    <app-button :name="'Reset Filter'"></app-button>
     <hr/>
-    <app-button :name="'Reset Filter'" orange></app-button>
+    <app-button :name="'Reset'" :variant="'orange'"></app-button>
     <hr/>
-    <app-button :name="'chekout'" uppercase bordered></app-button>
+    <app-button :name="'remove'" :variant="'red'" :tt="'uppercase'"></app-button>
     <hr/>
-    <app-button ></app-button>
+    <app-button icon :icon-src="require('@/assets/logo.png')" :variant="true ? 'icon-active' : ''"></app-button>
+    <app-button icon :icon-src="require('@/assets/logo.png')" style="margin-left: 10px;"></app-button>
+    <hr/>
+    <app-button :name="'add to cart'" :tt="'uppercase'" :variant="'colored'" :pdng="'23px 63px'" :br="'12px'" style="margin-right: 10px;"></app-button>
+    <app-button :name="'buy'" :tt="'uppercase'" :variant="'bordered'" :pdng="'23px 63px'" :br="'12px'"></app-button>
+    <hr/>
+    <app-button :name="'14-inch'" :variant="'variant-select' + true ? 'variant-select-active' : ''" :pdng="'23px 35px'" :br="'12px'"></app-button>
+    <app-button :name="'24-inch'" :variant="'variant-select'" style="margin-left: 10px;" :pdng="'23px 35px'" :br="'12px'"></app-button>
+    <hr/>
+    <app-button :name="'white'" :variant="'filter-select' + true ? 'filter-select-active' : ''" :pdng="'16px 32px'" :br="'7px'"></app-button>
+    <app-button :name="'black'" :variant="'filter-select'" style="margin-left: 10px;" :pdng="'16px 32px'" :br="'7px'"></app-button>
+    <hr/>
+    <app-button :name="'Add to cart'" :variant="'bordered'" :pdng="'18px 92px'" :br="'12px'" :bw="'2px'"></app-button>
   </div>
 </template>
 
@@ -23,7 +35,7 @@ export default {
 
 <style>
 .wrapper{
-  padding: 100 px;
+  padding: 100px;
   max-width: 700px;
   margin: 0 auto;
 }
--- compon/src/components/AppButton.vue
+++ compon/src/components/AppButton.vue
@@ -1,106 +1,163 @@
 <template>
-    <button type="button" 
-            @click="click" 
-            :class="{'colored': colored, 
-                     'uppercase': uppercase,
-                     'bordered' : bordered,
-                     'orange' : orange,
-                     'icon' : icon.length>0}">
-        {{name}}
-        <img v-if="icon.length>0" :src="icon">
-    </button>
+  <button type="button"
+          @click="click"
+          class="stndt-btn"
+          :class="[{icon: icon}, variant]"
+          :style="{fontsize: fs + 'px',
+                   lineHeight: lh + 'px',
+                   textTransform: tt,
+                   padding: pdng,
+                   borderRadius: br,
+                   borderWidth: bw}">
+    {{ name }}
+    <img v-if="icon" :src="iconSrc">
+  </button>
 </template>
 
 <script>
 export default {
-    name: 'AppButton',
-    props:{
-        name: {
-            type: String,
-            default: ''
-        },
-        icon: {
-            type: String,
-            default: ''
-        },
-        uppercase:{
-            type: Boolean,
-            default: false
-        },
-        colored:{
-            type: Boolean,
-            default: false
-        },
-        filter:{
-            type: Boolean,
-            default: false
-        },
-        bordered:{
-            type: Boolean,
-            default: false
-        },
-        orange:{
-            type: Boolean,
-            default: false
-        },
+  name: 'AppButton',
+  props: {
+    name: {
+      type: String,
+      default: ''
     },
-    methods:{
-        click(){
-            this.$emit('btnClick');
-        }
+    variant: {
+      type: String,
+      default: ''
+    },
+    icon:{
+      type: Boolean,
+      default: false
+    },
+    iconSrc: {
+      type: String,
+      default: ''
+    },
+    fs: Number,
+    lh: Number,
+    tt: String,
+    pdng: String,
+    br:{
+      type: String,
+      default: 'auto'
+    },
+    bw:{
+      type: String,
+      default: 'auto'
     }
+  },
+  methods: {
+    click() {
+      this.$emit('btnClick');
+    }
+  }
 }
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap');
 
-button{
-font-family: 'Lato';
-font-style: normal;
-font-weight: 700;
-font-size: 18px;
-line-height: 22px;
-color: #7B7B7B;
-background: none;
-border: none;
-padding: 0;
-margin: 0;
+.stndt-btn {
+  font-family: 'Lato';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 22px;
+  color: #7B7B7B;
+  background: none;
+  border: none;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
 }
-button:hover, button:active{
-    cursor: pointer;
-    color:#FF7020;
+.stndt-btn:hover, .stndt-btn:active {
+  color: #FF7020;
 }
 
 .orange{
-    color: #FF7020;
+  color: #FF7020;
+}
+.orange:hover{
+  color: #7B7B7B;
 }
 
-.uppercase{
-    text-transform: uppercase;
+.red{
+  color: #FF3535;
+}
+.red:hover{
+  color: #7B7B7B;
 }
 
+.icon,
+.icon-active,
+.colored,
+.bordered,
+.variant-select,
+.variant-select-active,
+.filter-select,
+.filter-select-active
+{
+  border: 1px solid;
+}
+
+.icon{
+  border: 1px solid #D8D8D8;
+  border-radius: 12px;
+  padding: 15px;
+  line-height: 0;
+}
+.icon:hover,
+.icon-active{
+  background: #FF7020;
+  border-color: #ff7020;
+}
 .icon img{
-    max-width: 22px;
+  max-height: 22px;
 }
 
-.icon {
-    max-width: 60px;
-    max-height: 56px;
+.colored{
+  background: #FF7020;
+  color: #FBFBFB;
+}
+.colored:hover{
+  border-color: #FF7020;
+  color: #FF7020;
+  background: #FBFBFB;
 }
 
-.icon:hover{
-    border-color: #FF7020;
+.bordered{
+  background: #FBFBFB;
+  color: #FF7020;
+}
+.bordered:hover{
+  border-color: #FF7020;
+  color: #FBFBFB;
+  background: #FF7020;
 }
 
-.icon:active{
-    background-color: #FF7020;
+.variant-select{
+  color: #7B7B7B;
+  border-color: #FFEADE;
+}
+.variant-select-active,
+.variant-select:hover{
+  background: rgba(255, 112, 32, 0.15);
+  border-color: #ffeade;
+  color: #FF7020;
 }
 
-.bordered {
-    border: 1px solid #D8D8D8;
-    border-radius: 12px;
-    padding: 19px;
-    
+.filter-select{
+  border-color: #FFD6BE;
+  color: #FF7020;
 }
+.filter-select-active,
+.filter-select:hover{
+  background: #FFE6D9;
+  border-color: #FFE6D9;
+  color: #FF7020;
+}
+
+
+
 </style>
